0b13b3105b8a0b8623d72dfe06ae9d41b0efe317
Fernando Fresquer <fernando@fresquer.dev>
2022-09-30 13:59:03 +0200

    User menu added

---
+++ interfaces/index.ts
@@ -0,0 +1,24 @@
+export type UserName = {
+    lastName: string,
+    firstName: string
+}
+
+export type TrankInfoModel = {
+    id: string,
+    createdAt: string,
+    updatedAt: string,
+    workEntryIn: WorkEntry,
+    workEntryOut: WorkEntry,
+    employee: Employee,
+}
+
+type WorkEntry = {
+    date: string;
+}
+
+type Employee = {
+    lastName: string,
+    firstName: string,
+    id: string,
+    workStatus: string,
+}
